4afdd08a090aa39bfd01377686bea527348ef7c5
artmadeit <this.arthur.email@gmail.com>
2019-07-12 00:57:38 -0500

    feat: add is distribution function

---
+++ spec/distributions_spec.cr
@@ -0,0 +1,17 @@
+require "./spec_helper"
+
+X = [2, 3, 4, 5, 6]
+describe DistributionFunction do
+  # See book: Statistical analysis for Engineers and Scientists
+
+  describe "f(x) = 5.2551 / x ** 3" do
+    it "#is" do
+      DistributionFunction.is?(X){ |x| 5.2551 / x ** 3 }.should be_true
+    end
+  end
+  describe "f(x) = 10 / x ** 3" do
+    it "#is not" do
+      DistributionFunction.is?(X){ |x| 10.0 / x ** 3 }.should be_false
+    end
+  end
+end
---
+++ src/distributions.cr
@@ -0,0 +1,12 @@
+class DistributionFunction
+  def self.is?(x : Array, delta = 0.01, &p)
+    x.all? { |xi| (yield xi) >= 0 } &&
+    Math.equals(x.sum { |xi| yield xi }, 1, delta)
+  end
+end
+
+module Math
+  def equals(n1, n2, delta)
+    (n1 - n2).abs <= delta
+  end
+end
