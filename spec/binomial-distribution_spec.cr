65fc75ebfff3285003f13ebd0bd1f0836dd8f5f6
artmadeit <this.arthur.email@gmail.com>
2019-09-09 20:43:21 -0500

    feat: constrain binomial distribution

--- spec/binomial-distribution_spec.cr
+++ spec/binomial-distribution_spec.cr
@@ -5,4 +5,17 @@ describe "binomial distribution" do
     p = binomial_distribution(trials: 5, success_probability: 0.1)
     p.call(2).should be_close(0.0729, 0.0001)
   end
+  it "fails on a success_probability less than 0 or greater than 1" do
+    expect_raises(ArgumentError, "success_probability must be between 0 and 1, success_probability: -1.0") do
+      p = binomial_distribution(trials: 5, success_probability: -1.0)
+    end
+    expect_raises(ArgumentError, "success_probability must be between 0 and 1, success_probability: 1.2") do
+      p = binomial_distribution(trials: 5, success_probability: 1.2)
+    end
+  end
+  it "fails on a negative trials" do
+    expect_raises(ArgumentError, "trials >= 0, trials: -1") do
+      p = binomial_distribution(trials: -1, success_probability: 0.1)
+    end
+  end
 end
--- src/binomial-distribution.cr
+++ src/binomial-distribution.cr
@@ -1,6 +1,10 @@
 def binomial_distribution(trials : Int32, success_probability : Float)
   n = trials
   p = success_probability
+
+  raise ArgumentError.new("success_probability must be between 0 and 1, success_probability: #{p}") unless 0 <= p  && p <= 1
+  raise ArgumentError.new("trials >= 0, trials: #{n}") unless n >= 0
+
   q = 1 - p
 
   ->(x : Int32){ n.combination(x) * (p ** x) * (q ** (n - x))}
